4045fa7c61c24a678f15f120f9e0b1a68803388f
Edilo <edilo13@gmail.com>
2023-05-05 20:13:35 -0400

    config retrofit

--- app/src/main/java/com/edilo/retrofit/Results.java
+++ app/src/main/java/com/edilo/retrofit/Results.java
@@ -1,4 +1,12 @@
 package com.edilo.retrofit;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Results {
+    @SerializedName("name")
+    private String superName;
+
+    public Results(String name) { this.superName = name; }
+
+    public String getName() { return superName; }
 }
